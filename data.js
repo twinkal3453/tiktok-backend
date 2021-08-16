89eddb8d1148b23864059fd6ec5552fa38e61414
Twinkal Kumar Raj <twinkalkrraj@gmail.com>
2021-08-16 18:52:42 +0530

    first upload

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    url: "https://www.youtube.com/embed/d2xhRxxW_5w",
+    channel: "Twinkal_daily",
+    description: "think beyond the limits",
+    song: "mar javan",
+    likes: "200",
+    messages: "150",
+    shares: "100",
+  },
+  {
+    url: "https://www.youtube.com/embed/d2xhRxxW_5w",
+    channel: "Twinkal_daily",
+    description: "think beyond the limits",
+    song: "mar javan",
+    likes: "200",
+    messages: "150",
+    shares: "100",
+  },
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,58 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import mongoose from "mongoose";
+
+import Data from "./data.js";
+import Videos from "./dbModel.js";
+
+const app = express();
+const port = process.env.PORT;
+
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+// db config
+const connection_url = process.env.URL;
+
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+// api endpoints
+app.get("/", (req, res) => res.status(200).send("hello twinkal"));
+
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
+
+app.get("/v2/posts", (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/v2/posts", (req, res) => {
+  const dbVideos = req.body;
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+// listener
+app.listen(port, () => console.log(`listening on port ${port}`));
